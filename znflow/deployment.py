3cf6bbbdf713045428b76823f7f7239ca233bea8
PythonFZ <fabian.zills@web.de>
2023-02-09 08:29:52 +0100

    improve dask support

--- znflow/deployment.py
+++ znflow/deployment.py
@@ -4,7 +4,7 @@ import uuid
 
 from dask.distributed import Client, Future
 
-from znflow.base import NodeBaseMixin
+from znflow.base import Connection, NodeBaseMixin
 from znflow.graph import DiGraph
 from znflow.utils import IterableHandler
 
@@ -18,6 +18,15 @@ class _LoadNode(IterableHandler):
         return value
 
 
+class _UpdateConnections(IterableHandler):
+    def default(self, value, **kwargs):
+        predecessors = kwargs["predecessors"]
+        if isinstance(value, Connection):
+            # We don't actually need the connection, we need the results.
+            return dataclasses.replace(value, instance=predecessors[value.uuid]).result
+        return value
+
+
 def node_submit(node, *args, **kwargs):
     # TODO you need to update all connections to the kwargs from previous
     #  dask futures. Maybe give a Connection a UUID as well?
@@ -25,6 +34,22 @@ def node_submit(node, *args, **kwargs):
 
     # TODO look https://distributed.dask.org/en/stable/actors.html
     #  probably not want you want.
+    predecessors = kwargs.get("predecessors", {})
+    # if predecessors:
+    #    raise ValueError(predecessors)
+    for item in dir(node):
+        if item.startswith("_"):
+            continue
+        try:
+            # TODO how to only do this if something changed?
+            setattr(
+                node,
+                item,
+                _UpdateConnections()(getattr(node, item), predecessors=predecessors),
+            )
+        except Exception:
+            pass
+
     node.run()
     return node
 
@@ -59,9 +84,13 @@ class Deployment:
                 self.results[node.uuid] = self.client.submit(
                     node_submit,
                     node=node,
-                    *(self.results[x] for x in self.results if x in predecessors),
+                    predecessors={
+                        x: self.results[x] for x in self.results if x in predecessors
+                    },
                     pure=False,
                 )
 
     def get_results(self, obj: typing.Union[NodeBaseMixin, list, dict], /):
+        if isinstance(obj, DiGraph):
+            return _LoadNode()(dict(obj.nodes), results=self.results)
         return _LoadNode()(obj, results=self.results)
