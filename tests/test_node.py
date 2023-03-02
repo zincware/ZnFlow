17ca6c793b7a445fb3a98e01c205fcfcad72289e
PythonFZ <fabian.zills@web.de>
2023-03-02 14:32:37 +0100

    fix issue with properties that return Connections

--- tests/test_node.py
+++ tests/test_node.py
@@ -15,6 +15,10 @@ class PlainNode(znflow.Node):
     def run(self):
         self.value += 1
 
+    @property
+    def output(self):
+        return znflow.get_attribute(self, "value")
+
 
 @dataclasses.dataclass
 class DataclassNode(znflow.Node):
@@ -23,12 +27,20 @@ class DataclassNode(znflow.Node):
     def run(self):
         self.value += 1
 
+    @property
+    def output(self):
+        return znflow.get_attribute(self, "value")
+
 
 class ZnInitNode(zninit.ZnInit, znflow.Node):
     value: int = zninit.Descriptor()
 
     def run(self):
         self.value += 1
+
+    @property
+    def output(self):
+        return znflow.get_attribute(self, "value")
 
 
 @attrs.define
@@ -37,6 +49,10 @@ class AttrsNode(znflow.Node):
 
     def run(self):
         self.value += 1
+
+    @property
+    def output(self):
+        return znflow.get_attribute(self, "value")
 
 
 @znflow.nodify
--- znflow/graph.py
+++ znflow/graph.py
@@ -1,4 +1,6 @@
+import functools
 import logging
+import typing
 
 import networkx as nx
 
@@ -11,42 +13,41 @@ log = logging.getLogger(__name__)
 
 class _AttributeToConnection(utils.IterableHandler):
     def default(self, value, **kwargs):
-        v_attr = kwargs.get("v_attr")
-        node_instance = kwargs["node_instance"]
-        graph = kwargs["graph"]
-
+        assert not kwargs
         if isinstance(value, FunctionFuture):
-            connection = Connection(value, attribute="result")
-            if v_attr is None:
-                graph.add_connections(connection, node_instance)
-            else:
-                graph.add_connections(connection, node_instance, v_attr=v_attr)
-
-            return connection
+            return Connection(value, attribute="result")
         elif isinstance(value, Node):
-            connection = Connection(value, attribute=None)
-            if v_attr is None:
-                graph.add_connections(connection, node_instance)
-            else:
-                graph.add_connections(connection, node_instance, v_attr=v_attr)
-            return connection
-        elif isinstance(value, Connection):
+            return Connection(value, attribute=None)
+        else:
+            return value
+
+
+class _AddConnectionToGraph(utils.IterableHandler):
+    def default(self, value, **kwargs):
+        if isinstance(value, Connection):
+            graph = kwargs["graph"]
+            v_attr = kwargs.get("attribute")
+            node_instance = kwargs["node_instance"]
             if v_attr is None:
                 graph.add_connections(value, node_instance)
             else:
                 graph.add_connections(value, node_instance, v_attr=v_attr)
-            return value
-        return value
 
 
 class _UpdateConnectors(utils.IterableHandler):
     def default(self, value, **kwargs):
-        if isinstance(value, Connection):
-            return value.result
-        return value
+        return value.result if isinstance(value, Connection) else value
 
 
 class DiGraph(nx.MultiDiGraph):
+    @property
+    def add_connections_from_iterable(self) -> typing.Callable:
+        """Get a function that adds connections to the graph.
+
+        Return a _AddConnectionToGraph class with this Graph instance attached.
+        """
+        return functools.partial(_AddConnectionToGraph(), graph=self)
+
     def __enter__(self):
         if get_graph() is not None:
             raise ValueError("DiGraph already exists. Nested Graphs are not supported.")
@@ -63,28 +64,37 @@ class DiGraph(nx.MultiDiGraph):
             node_instance = self.nodes[node]["value"]
             log.debug(f"Node {node} ({node_instance}) was added to the graph.")
             if isinstance(node_instance, FunctionFuture):
-                node_instance.args = _AttributeToConnection()(
-                    node_instance.args, node_instance=node_instance, graph=self
-                )
-                node_instance.kwargs = _AttributeToConnection()(
-                    node_instance.kwargs, node_instance=node_instance, graph=self
-                )
+                self._update_function_future_arguments(node_instance)
             elif isinstance(node_instance, Node):
                 self._update_node_attributes(node_instance, _AttributeToConnection())
 
-    def _update_node_attributes(self, node_instance, updater):
+    def _update_function_future_arguments(self, node_instance: FunctionFuture) -> None:
+        """Apply an update to args and kwargs of a FunctionFuture."""
+        node_instance.args = _AttributeToConnection()(node_instance.args)
+        node_instance.kwargs = _AttributeToConnection()(node_instance.kwargs)
+        self.add_connections_from_iterable(
+            node_instance.args, node_instance=node_instance
+        )
+        self.add_connections_from_iterable(
+            node_instance.kwargs, node_instance=node_instance
+        )
+
+    def _update_node_attributes(self, node_instance: Node, updater) -> None:
         """Apply an updater to all attributes of a node."""
         for attribute in dir(node_instance):
             if attribute.startswith("_") or attribute in Node._protected_:
+                # We do not allow connections to private attributes.
                 continue
-            value = updater(
-                getattr(node_instance, attribute),
-                node_instance=node_instance,
-                graph=self,
-                v_attr=attribute,
-            )
+            value = getattr(node_instance, attribute)
+            value = updater(value)
             if updater.updated:
-                setattr(node_instance, attribute, value)
+                try:
+                    setattr(node_instance, attribute, value)
+                except AttributeError:
+                    continue
+            self.add_connections_from_iterable(
+                value, node_instance=node_instance, attribute=attribute
+            )
 
     def add_node(self, node_for_adding, **attr):
         if isinstance(node_for_adding, NodeBaseMixin):
