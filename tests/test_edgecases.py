1258e79baa90711d0035b622e8deef63a76fbfc3
PythonFZ <fabian.zills@web.de>
2023-03-17 18:06:34 +0100

    add docstring

--- tests/test_edgecases.py
+++ tests/test_edgecases.py
@@ -2,6 +2,11 @@ import znflow
 
 
 class PlainNode(znflow.Node):
+    """A Node with the 'result' attribute.
+
+    'result' had been used internally before.
+    """
+
     @property
     def result(self):
         raise TypeError("This value is not available until the node is run.")
