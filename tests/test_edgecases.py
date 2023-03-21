8b5a5c3a6a3b76b9f59cb6d580c2607f3d467c2e
PythonFZ <fabian.zills@web.de>
2023-03-21 14:30:37 +0100

    add misc tests

--- tests/test_edgecases.py
+++ tests/test_edgecases.py
@@ -12,9 +12,35 @@ class PlainNode(znflow.Node):
         raise TypeError("This value is not available until the node is run.")
 
 
+class ConnectResults(znflow.Node):
+    @property
+    def result(self):
+        return 42
+
+    def run(self):
+        pass
+
+
+@znflow.nodify
+def add_one(x):
+    return x + 1
+
+
 def test_PlainNode():
     with znflow.DiGraph():
         node1 = PlainNode()
         con = node1.result
 
     assert isinstance(con, znflow.Connection)
+
+
+def test_ConnectResults():
+    with znflow.DiGraph() as graph:
+        node1 = ConnectResults()
+        con = node1.result
+        outs = add_one(con)
+
+    assert isinstance(con, znflow.Connection)
+
+    graph.run()
+    assert outs.result == 43
