a9d14bef98bd129a32ed5326f3d646eb0bdbd8ef
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-02-27 14:55:37 +0100

    cleanup by copilot

--- znflow/deployment/vanilla.py
+++ znflow/deployment/vanilla.py
@@ -11,33 +11,29 @@ class VanillaDeployment(DeploymentBase):
 
     def run(self, nodes: t.Optional[t.List] = None):
         for node_uuid in self.graph.get_sorted_nodes():
-            if self.graph.immutable_nodes and self.graph.nodes[node_uuid].get(
-                "available", False
-            ):
-                continue
             node = self.graph.nodes[node_uuid]["value"]
-            if nodes is None:
-                if not node._external_:
-                    self.graph._update_node_attributes(node, handler.UpdateConnectors())
-                    node.run()
-                    if self.graph.immutable_nodes:
-                        self.graph.nodes[node_uuid]["available"] = True
-            else:
-                if node in nodes:
-                    predecessors = list(self.graph.predecessors(node_uuid))
-                    for predecessor in predecessors:
-                        predecessor_node = self.graph.nodes[predecessor]["value"]
-                        if self.graph.immutable_nodes and self.graph.nodes[
-                            predecessor
-                        ].get("available", False):
-                            continue
-                        self.graph._update_node_attributes(
-                            predecessor_node, handler.UpdateConnectors()
-                        )
-                        predecessor_node.run()
-                        if self.graph.immutable_nodes:
-                            self.graph.nodes[predecessor]["available"] = True
-                    self.graph._update_node_attributes(node, handler.UpdateConnectors())
-                    node.run()
-                    if self.graph.immutable_nodes:
-                        self.graph.nodes[node_uuid]["available"] = True
+            node_available = self.graph.nodes[node_uuid].get("available", False)
+
+            if self.graph.immutable_nodes and node_available:
+                continue
+
+            if nodes is None and not node._external_:
+                self._run_node(node, node_uuid)
+            elif nodes is not None and node in nodes:
+                self._run_predecessors(node_uuid)
+                self._run_node(node, node_uuid)
+
+    def _run_node(self, node, node_uuid):
+        self.graph._update_node_attributes(node, handler.UpdateConnectors())
+        node.run()
+        if self.graph.immutable_nodes:
+            self.graph.nodes[node_uuid]["available"] = True
+
+    def _run_predecessors(self, node_uuid):
+        predecessors = list(self.graph.predecessors(node_uuid))
+        for predecessor in predecessors:
+            predecessor_node = self.graph.nodes[predecessor]["value"]
+            predecessor_available = self.graph.nodes[predecessor].get("available", False)
+
+            if not (self.graph.immutable_nodes and predecessor_available):
+                self._run_node(predecessor_node, predecessor)
