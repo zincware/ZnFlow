48636f4a36a51c2e5ab88af752496313e0cad5cd
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2024-10-17 15:54:38 +0200

    test `__post_init__` (#114)
    * test `__post_init__`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * replace with typing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/test_post_init.py
@@ -0,0 +1,29 @@
+import dataclasses
+import znflow
+import typing as t
+
+
+@dataclasses.dataclass
+class NodeWithPostInit(znflow.Node):
+    input: int
+    output: t.Optional[int] = None
+
+    def __post_init__(self):
+        self.input = self.input * 2
+
+    def run(self):
+        self.output = self.input + 1
+
+
+def test_post_init():
+    graph = znflow.DiGraph()
+
+    with graph:
+        node = NodeWithPostInit(input=10)
+
+    assert node.input == 20
+
+    graph.run()
+
+    assert node.output == 21
+    assert node.input == 20
