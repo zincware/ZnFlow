3a8b36cb5d1734ab43aa71f77c1a8ac6b552dc72
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-10-17 13:09:34 +0200

    test `__post_init__`

---
+++ tests/test_post_init.py
@@ -0,0 +1,28 @@
+import dataclasses
+import znflow
+
+@dataclasses.dataclass
+class NodeWithPostInit(znflow.Node):
+    input: int
+    output: int|None = None
+    
+
+    def __post_init__(self):
+        self.input = self.input * 2
+
+    def run(self):
+        self.output = self.input + 1
+
+
+def test_post_init():
+    graph = znflow.DiGraph()
+    
+    with graph:
+        node = NodeWithPostInit(input=10)
+
+    assert node.input == 20
+    
+    graph.run()
+
+    assert node.output == 21
+    assert node.input == 20
