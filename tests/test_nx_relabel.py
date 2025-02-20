318c97d591ce95e74f369bf00fce85bad3533f98
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-02-20 11:15:40 +0100

    test relabel feature

---
+++ tests/test_nx_relabel.py
@@ -0,0 +1,22 @@
+import dataclasses
+import znflow
+import networkx as nx
+
+@dataclasses.dataclass
+class DataclassNode(znflow.Node):
+    value: int
+
+    def run(self):
+        self.value += 1
+
+
+def test_nx_relabel_nodes():
+    graph = znflow.DiGraph()
+
+    with graph:
+        node = DataclassNode(value=42)
+    
+    assert node.uuid in graph
+    nx.relabel_nodes(graph, {node.uuid: "custom"}, copy=False)
+    assert "custom" in graph
+    assert node.uuid not in graph
