83ac75b8a8355978e1c1222d0c8299fb022cb93e
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-09-11 15:04:09 +0200

    promote fix

--- znflow/base.py
+++ znflow/base.py
@@ -187,7 +187,9 @@ class Connection:
     def __add__(
         self, other: typing.Union[Connection, FunctionFuture, CombinedConnections]
     ) -> CombinedConnections:
-        if isinstance(other, (Connection, FunctionFuture, CombinedConnections)):
+        if isinstance(other, CombinedConnections):
+            return CombinedConnections(connections=[self] + other.connections)
+        if isinstance(other, (Connection, FunctionFuture)):
             return CombinedConnections(connections=[self, other])
         raise TypeError(f"Can not add {type(other)} to {type(self)}.")
 
